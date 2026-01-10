0af2486295cb4e138d75c39beb4a625bf2691345
TingLei-daprediction <leiting2002@gmail.com>
2026-01-10 15:22:16 +0000

    WIP

--- scripts/run_check_era5_vars.sh
+++ scripts/run_check_era5_vars.sh
@@ -7,7 +7,7 @@ INPUT_DIR="/scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-weatherbench/era5_1p5deg/pressu
 YEAR=2007
 PATTERN="era5_*_{year}_*.nc"
 
-python scripts/check_era5_vars.py \
+python check_era5_vars.py \
   --input_dir "${INPUT_DIR}" \
   --year "${YEAR}" \
   --pattern "${PATTERN}"
