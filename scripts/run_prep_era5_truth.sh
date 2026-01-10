60752d6c274a603d69a2d02d334222e0a719095e
TingLei-daprediction <leiting2002@gmail.com>
2026-01-09 23:25:29 -0500

    modified run_prep_era5_truth.sh to run mode of 4u_sfc

--- scripts/run_prep_era5_truth.sh
+++ scripts/run_prep_era5_truth.sh
@@ -12,10 +12,17 @@ YEARS=(2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019)
 #clt TIME_FREQ="6H"  # use "1D" for daily 00 UTC outputs
 TIME_FREQ="1D"  # use "1D" for daily 00 UTC outputs
 
-# --- Surface/single-level fields (example) ---
+# --- Combined 4u_sfc (pressure-level + surface) ---
+INPUT_DIR_PL="/scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-weatherbench/era5_1p5deg/pressure_level"
 INPUT_DIR_SFC="/scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-weatherbench/era5_1p5deg/single_level"
-ERA5_MODE_SFC="sfc"
-VARS_SFC=(
+ERA5_MODE="4u_sfc"
+VARS=(
+  geopotential
+  temperature
+  relative_humidity
+  u_component_of_wind
+  v_component_of_wind
+  specific_humidity
   2m_temperature
   2m_dewpoint_temperature
   10m_u_component_of_wind
@@ -24,34 +31,12 @@ VARS_SFC=(
   surface_pressure
 )
 
-# --- Pressure-level fields (example) ---
-# Requires downloading reanalysis-era5-pressure-levels with these variables/levels.
-INPUT_DIR_PL="/scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-weatherbench/era5_1p5deg/pressure_level"
-ERA5_MODE_PL="4u"
-VARS_PL=(
-  geopotential
-  temperature
-  specific_humidity
-  u_component_of_wind
-  v_component_of_wind
-)
-
-# Run surface
-python ${scriptdir}/prep_era5_truth.py \
-  --input_dir "${INPUT_DIR_SFC}" \
-  --output_dir "${OUTPUT_DIR}" \
-  --era5_mode "${ERA5_MODE_SFC}" \
-  --variables "${VARS_SFC[@]}" \
-  --years "${YEARS[@]}" \
-  --grid_dir "${GRID_DIR}" \
-  --time_freq "${TIME_FREQ}"
-
-# Run pressure-level
 python ${scriptdir}/prep_era5_truth.py \
   --input_dir "${INPUT_DIR_PL}" \
+  --sfc_input_dir "${INPUT_DIR_SFC}" \
   --output_dir "${OUTPUT_DIR}" \
-  --era5_mode "${ERA5_MODE_PL}" \
-  --variables "${VARS_PL[@]}" \
+  --era5_mode "${ERA5_MODE}" \
+  --variables "${VARS[@]}" \
   --years "${YEARS[@]}" \
   --grid_dir "${GRID_DIR}" \
   --time_freq "${TIME_FREQ}"
